bf4621002044d0d361191cf5d7ff68626b4a8dda
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-04-29 14:46:51 -0500

    Use var everywhere for consistency

--- clickarea.cs
+++ clickarea.cs
@@ -28,7 +28,7 @@ public partial class ClickArea : Area2D
         // Bodies are fine, it can be useful for animals later on
         var list = GetOverlappingBodies();
         var other = GetOverlappingAreas();
-        List<Node2D> nodesClicked = new();
+        var nodesClicked = new List<Node2D>();
         var player = (Player) GetParent().GetNode("Player");
 
         foreach (var node in list)
